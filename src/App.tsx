b877acafdddc65850f4f43f6f42a7d0e4e956fd4
daimori <anraku90@gmail.com>
2019-12-15 05:45:20 +0900

    だいたい実装完了

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,11 @@
 import React from 'react'
 import Hangman from './components/Hangman'
-import styled from 'styled-components'
-
-const Container = styled.div`
-  text-align: center;
-`
 
 const App: React.FC = () => {
   return (
-    <Container>
+    <>
       <Hangman />
-    </Container>
+    </>
   )
 }
 
--- src/components/Hangman.tsx
+++ src/components/Hangman.tsx
@@ -1,26 +1,102 @@
 import React, { useState, useEffect } from 'react'
-import HangmanImage from './HangmanImage'
+import styled from 'styled-components'
+import { ENGLISH_WORDS } from '../utils/words'
 import img1 from '../images/1.jpg'
 import img2 from '../images/2.jpg'
 import img3 from '../images/3.jpg'
 import img4 from '../images/4.jpg'
 import img5 from '../images/5.jpg'
 import img6 from '../images/6.jpg'
-import KeyContext from '../contexts/App'
-import AnswerString from './AnswerString'
-import KeyBoard from './KeyBoard'
-import { useAppData } from '../hooks/App'
+
+const Container = styled.div`
+  text-align: center;
+`
+
+const GuessString = styled.p`
+  padding-left: 1rem;
+  letter-spacing: 1rem;
+  font-size: 2rem;
+`
+
+const KeyBoard = styled.div`
+  display: flex;
+  justify-content: space-evenly;
+  flex-flow: row wrap;
+  width: 400px;
+  margin: auto;
+  margin-top: 200px;
+`
+
+const Button = styled.button`
+  display: inline-block;
+  width: 40px;
+  height: 40px;
+  margin-top: 10px;
+  margin-right: 10px;
+  color: #ffffff;
+  font-size: 20px;
+  border-radius: 5px;
+  background-color: #ecc000;
+
+  :disabled {
+    background-color: #cccccc;
+  }
+`
+
+const Reset = styled(Button)`
+  width: auto;
+`
 
 const Hangman: React.FC = () => {
-  const images = [img1, img2, img3, img4, img5, img6]
-  const {key, setKey, random, answer, setAnswer}= useAppData()
+  const images = [img1, img1, img2, img3, img4, img5, img6]
+  const [guessed, setGuessed] = useState<string[]>([])
+  const [answer, setAnswer] = useState("")
+  const [guessedCount, setGuessedCount] = useState(0)
+  const guessString = () => answer.split("").map(c => guessed.includes(c) ? c : "_")
+  const randomString = () => ENGLISH_WORDS[(Math.floor(Math.random() * ENGLISH_WORDS.length))]
+  
+  useEffect(() => {
+    setAnswer(randomString())
+  }, [])
 
+  const handleGuess = (e: any) => {
+    e.preventDefault()
+    const value = e.target.value
+    setGuessed(guessed.concat(value))
+    if (!answer.includes(value)) {
+      setGuessedCount(prev => prev + 1)
+    }
+  }
+
+  const handleReset = () => {
+    setGuessed([])
+    setGuessedCount(0)
+    setAnswer(randomString())
+  }
+
+  const gameOver = guessedCount > 5
+  const isWin = answer === guessString().join('')
+  let gameState
+  if (gameOver) gameState = `You Lose Answer is ${answer}`
+  if (isWin) gameState = 'You Win'
   return (
-    <KeyContext.Provider value={{ key, setKey }}>
-      <HangmanImage images={images}/>
-      <AnswerString random={random} answer={answer} />
-      <KeyBoard />
-    </KeyContext.Provider>
+    <Container>
+      <img src={images[guessedCount]} alt="Hangman"/>
+      <GuessString>
+        {guessString()}
+      </GuessString>
+      {!isWin && !gameOver ?
+        <KeyBoard>
+          {"abcdefghijklmnopqrstuvwxyz".split('').map((word, index) =>
+            <Button key={index} value={word} onClick={handleGuess} disabled={guessed.includes(word)}>
+              {word}
+            </Button>
+          )}
+        </KeyBoard>
+        : <p>{gameState}</p>
+      }
+      <Reset onClick={handleReset}>Reset</Reset>
+    </Container>
   )
 }
 
